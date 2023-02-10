575947ef0c6bcc66548b1012e7560147d9f641a3
Joe Previte <jjprevite@gmail.com>
2023-02-10 05:52:58 +0000

    feat: add reform

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -1,18 +1,21 @@
-import React from "react"
-
 export function CTA() {
-    const [name, setName] = React.useState("")
-    const [email, setEmail] = React.useState("")
-
     return (
         <div className="text-center text-primary my-24">
             <h3 className="text-5xl leading-relaxed tracking-tighter">Speak like a native</h3>
             <p className="text-sm mb-3">Reach your fluency goals 2x faster. Guaranteed.</p>
-            <form>
-                <input className="block mx-auto w-48 placeholder:text-slate-400 py-2 px-4 rounded-md mb-2" placeholder="Your name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
-                <input className="block mx-auto w-48 placeholder:text-slate-400 py-2 px-4 rounded-md mb-2" type="text" placeholder="you@example.com" value={email} onChange={(e) => setEmail(e.target.value)} />
-                <input className="text-center block bg-arg-gold b-arg-gold-dark text-black font-bold w-fit px-6 py-3 rounded-md mx-auto mt-4" type="submit" value="Join the waitlist" />
-            </form>
-        </div>
+            <form method="POST" action="https://forms.reform.app/headless/WR4x5f/arg-spanish-waitlist/c1zkky/submissions">
+                <div>
+                    <input
+                        className="block mx-auto w-48 placeholder:text-slate-400 py-2 px-4 rounded-md mb-2"
+                        type="email"
+                        id="12c97b2c-36a9-4f3f-9afb-8ee39992c2cd"
+                        name="answers[12c97b2c-36a9-4f3f-9afb-8ee39992c2cd]"
+                        placeholder="Enter your email"
+                        required
+                    />
+                </div>
+                <button className="text-center block bg-arg-gold b-arg-gold-dark text-black font-bold w-fit px-6 py-3 rounded-md mx-auto mt-4" type="submit">Join the waitlist</button>
+            </form >
+        </div >
     )
 }
