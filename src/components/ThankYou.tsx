da9c2bf0cd430d76b0e14377a8984d037fdbe66e
Joe Previte <jjprevite@gmail.com>
2023-02-12 17:01:21 +0000

    feat:  add messi gif to thank you

--- src/components/ThankYou.tsx
+++ src/components/ThankYou.tsx
@@ -23,6 +23,7 @@ export function ThankYou() {
     }, []);
     return <div className="mt-16 mb-48 text-primary">
         <h1 className="leading-relaxed text-center font-semibold tracking-tighter text-5xl mb-8">Thank you!</h1>
+        <img className="mb-8" src="https://media.giphy.com/media/W1TPavKSPEVVjz4LAJ/giphy.gif" />
         <p className="mb-8">It's great to have you on the waitlist. </p>
         <p className="mb-8">Once the waitlist has 100 signups, I will email you the course outline and ask for your feedback.</p>
 
