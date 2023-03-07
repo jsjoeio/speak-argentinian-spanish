cc8177334ab113c5bbc1bbb24a4a854af62344d5
Joe Previte <jjprevite@gmail.com>
2023-03-07 04:50:18 +0000

    get blog ready

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,12 +1,22 @@
 export function About() {
-    return (
-        <div className="mx-auto flex flex-row text-primary mb-12">
-            <img className="h-40" src="https://avatars.githubusercontent.com/u/3806031?v=4" alt="" />
-            <div className="ml-4">
-                <p className="mb-4">Hola, soy Joe 👋</p>
-                <p>I studied abroad in Buenos Aires in 2013. Now I’m teaching my daughter Argentinian Spanish 🇦🇷.
-                    I created this course because it’s something <em>I wish existed</em> when I was first starting. </p>
-            </div>
+  return (
+    <div className="pt-12 prose">
+      <hr />
+      <div className="flex flex-col space-y-4 md:space-y-0 md:space-x-6 md:flex-row">
+        <img
+          src="https://avatars.githubusercontent.com/u/3806031?v=4"
+          alt="avatar of Joe Previte"
+          className="self-center flex-shrink-0 w-24 h-24 border rounded-full md:justify-self-start dark:bg-gray-500 dark:border-gray-800 border-2"
+        />
+        <div className="flex flex-col">
+          <h4 className="text-lg font-semibold">Hola, soy Joe 👋</h4>
+          <p>
+            I studied abroad in Buenos Aires in 2013. Now I’m teaching my
+            daughter Argentinian Spanish 🇦🇷. I created this course because it’s
+            something <em>I wish existed</em> when I was first starting.{" "}
+          </p>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'arg-gold': '#d3d359',
-				'arg-gold-dark': '#8C7241',
-				'arg-white': '#f2f2f2',
-				'primary': '#222222'
-			}
-		},
-	},
-	plugins: [require("daisyui")],
-	daisyui: {
-		styled: true,
-		themes: false,
-		base: false,
-		utils: true,
-		logs: false,
-		rtl: false,
-	},
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "arg-gold": "#d3d359",
+        "arg-gold-dark": "#8C7241",
+        "arg-white": "#f2f2f2",
+        primary: "#222222",
+      },
+    },
+  },
+  plugins: [require("daisyui"), require("@tailwindcss/typography")],
+  daisyui: {
+    styled: true,
+    themes: false,
+    base: false,
+    utils: true,
+    logs: false,
+    rtl: false,
+  },
+};
