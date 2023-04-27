20c46c1e0a665aba80e1353e9ce381ccc6aa5d93
Joe Previte <jjprevite@gmail.com>
2023-04-27 04:21:20 +0000

    refactor: get totalCount from Beehiiv

--- src/pages/getWaitlistTotal.json.ts
+++ src/pages/getWaitlistTotal.json.ts
@@ -1,16 +1,23 @@
-import { Redis } from '@upstash/redis'
-
-const redis = new Redis({
-    url: import.meta.env.REDIS_URL,
-    token: import.meta.env.REDIS_TOKEN
-})
-
 export async function get({ params, request }) {
-    // const data = await redis.set('foo', 'bar');
-    const data = await redis.get("waitlistTotal")
+  const url =
+    "https://api.beehiiv.com/v2/publications/pub_6fcf5bfc-5793-49e6-b6b2-abcf322a6fd7/subscriptions?status=active&tier=all&limit=1";
+  const options = {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${import.meta.env.BEEHIIV_API_TOKEN}`,
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
     return {
-        body: JSON.stringify({
-            totalCount: data
-        }),
+      body: JSON.stringify({
+        totalCount: data.total_results,
+      }),
     };
+  } catch (error) {
+    console.error(error);
+  }
 }
