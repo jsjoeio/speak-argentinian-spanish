6bd6dd03633af4ce0dc51040970fc1c2cde290c0
Chris Pennington <chris@codinginpublic.dev>
2023-05-02 22:03:39 -0600

    update searchForm JSX styling

--- src/components/search/SearchForm.tsx
+++ src/components/search/SearchForm.tsx
@@ -12,16 +12,42 @@ const SearchForm = ({ query, setQuery }: Props) => {
         url.searchParams.set("q", query);
         window.history.replaceState(null, "", url.toString());
       }}
+      className="flex w-full max-w-lg"
     >
       <input
-        className="block w-full max-w-xs py-2 px-4 rounded-md bg-transparent border-2 border-arg-gold focus:outline-arg-gold placeholder:text-arg-muted"
+        className="w-full px-4 py-3 bg-white focus:outline-none hover:shadow-none focus:shadow-none transition-shadow rounded-l appearance-none border-2 border-arg-muted focus-visible:border-arg-gold peer placeholder:text-arg-muted shadow"
         type="search"
         name="search"
+        autoFocus
         placeholder="Enter Search Term…"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         required
       />
+      <label htmlFor="search-widget-input" className="sr-only">
+        Enter a search term
+      </label>
+      <button
+        type="submit"
+        className="bg-arg-muted peer-focus:bg-arg-gold shadow-md text-xs px-4 rounded-r text-arg-white uppercase focus:outline-none hover:shadow-none focus:shadow-none transition-shadow focus-visible:ring-4 ring-arg-dark cursor-pointer flex items-center gap-1"
+        aria-label="Submit search"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          className="feather feather-search"
+        >
+          <circle cx="11" cy="11" r="8"></circle>
+          <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+        </svg>
+      </button>
     </form>
   );
 };
