feafdfdcfa83638b785b64e6463e8189516fc5bb
Chris Pennington <chris@codinginpublic.dev>
2023-05-03 21:24:11 -0600

    remove queries from postSearch endpoint

--- src/pages/postSearch.json.ts
+++ src/pages/postSearch.json.ts
@@ -1,9 +1,8 @@
-import Fuse from "fuse.js";
 import { getCollection } from "astro:content";
 
 const posts = (await getCollection("blog")).sort(
   (a, b) => b.data.date.valueOf() - a.data.date.valueOf()
-); // is there a way to cache this?
+);
 
 const processedPosts = posts.map((post) => ({
   slug: post.slug,
@@ -14,45 +13,11 @@ const processedPosts = posts.map((post) => ({
   },
 }));
 
-const searchIndex = Fuse.createIndex(
-  ["data.title", "data.description", "data.date", "slug"],
-  processedPosts
-);
-
-export async function get({ request }) {
-  const url = new URL(request.url);
-  const query = url.searchParams.get("q") ?? "";
-  const limit = url.searchParams.get("limit");
-  const fuse = new Fuse(
-    processedPosts,
-    {
-      includeScore: true,
-      shouldSort: true,
-      minMatchCharLength: 2,
-      keys: [
-        {
-          name: "data.title",
-          weight: 3,
-        },
-        {
-          name: "data.description",
-          weight: 2,
-        },
-      ],
+export async function get({}) {
+  return new Response(JSON.stringify(processedPosts), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
     },
-    searchIndex
-  );
-  const filteredPosts = fuse.search(query);
-
-  return new Response(
-    limit
-      ? JSON.stringify(filteredPosts.slice(0, +limit))
-      : JSON.stringify(filteredPosts),
-    {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  });
 }
