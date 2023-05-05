07f0dcd19b0dea265f27c556f3a5788d03090b27
Joe Previte <jjprevite@gmail.com>
2023-05-05 13:41:34 -0700

    fix: remove waitlist copy from thank you

--- src/components/ThankYou.tsx
+++ src/components/ThankYou.tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import {
   EmailIcon,
   EmailShareButton,
@@ -16,10 +15,6 @@ import {
   WhatsappIcon,
 } from "react-share";
 
-type ResponseData = {
-  totalCount: number;
-};
-
 const URL = "https://speakargentinianspanish.com";
 const shortDescription = "Signup to learn Argentinian Spanish with me!";
 const longTitle = "Learn Argentinian Spanish with me!";
@@ -28,23 +23,6 @@ const longDescription =
 const shortTitle = "Learn Argentinian Spanish!";
 
 export function ThankYou() {
-  const [waitlistCount, setWaitlistCount] = useState(null);
-  useEffect(() => {
-    async function fetchWaitlistTotal() {
-      setWaitlistCount(null);
-      const res = await fetch("/getWaitlistTotal.json");
-      const data = (await res.json()) as ResponseData;
-      if (!ignore) {
-        setWaitlistCount(data.totalCount);
-      }
-    }
-
-    let ignore = false;
-    fetchWaitlistTotal();
-    return () => {
-      ignore = true;
-    };
-  }, []);
   return (
     <div className="mt-16 mb-48">
       <h1 className="leading-relaxed text-center font-semibold tracking-tighter text-5xl mb-8">
@@ -54,23 +32,11 @@ export function ThankYou() {
         className="mb-8"
         src="https://media.giphy.com/media/W1TPavKSPEVVjz4LAJ/giphy.gif"
       />
-      <p className="mb-8">It's great to have you on the waitlist. </p>
+      <p className="mb-8">It's great to have you in the community. </p>
       <p className="mb-8">
-        Once the waitlist has 100 signups, I will email you the course outline
-        and ask for your feedback.
+        <strong className="uppercase underline">important:</strong> Check your
+        email and confirm your signup.
       </p>
-
-      {waitlistCount ? (
-        <div className="mb-8">
-          <h2>Total Signups</h2>
-          <small>{waitlistCount}/100</small>
-          <progress
-            className="progress w-full"
-            value={waitlistCount}
-            max="100"
-          ></progress>
-        </div>
-      ) : null}
       <h2 className="leading-relaxed text-center font-semibold tracking-tighter text-4xl mb-8">
         Spread the word!
       </h2>
