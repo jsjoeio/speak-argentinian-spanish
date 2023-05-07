84f28b97f7f4c4fcac4fca4ac8cb4d6b3408dfc8
Joe Previte <jjprevite@gmail.com>
2023-05-07 19:16:28 +0000

    docs: update copy to match newsletter

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Speak Argentinian Spanish';
-export const SITE_DESCRIPTION = 'The best course for learning Argentinian Spanish.';
+export const SITE_TITLE = "Speak Argentinian Spanish";
+export const SITE_DESCRIPTION =
+  "The best newsletter for learning Argentinian Spanish.";
