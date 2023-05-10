3b9af8252121100e3691ba3d839a9cd162114c2e
Chris Pennington <chris@codinginpublic.dev>
2023-05-09 21:07:26 -0600

    move Formatted Date component logic to utils

--- src/utils/formatDates.ts
+++ src/utils/formatDates.ts
@@ -2,3 +2,11 @@ export function formatDate(date: Date) {
   date.setUTCHours(0);
   return date;
 }
+
+export function formatToLocaleDateString(date: Date) {
+  return date.toLocaleDateString("en-us", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+}
