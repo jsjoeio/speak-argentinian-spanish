485fb417e5ef998e24c50b87cbc36d7c5945e1e6
Chris Pennington <chris@codinginpublic.dev>
2023-05-25 20:18:49 -0600

    left footer section

--- src/consts.ts
+++ src/consts.ts
@@ -7,3 +7,16 @@ export const SITE_DESCRIPTION =
 
 export const SITE_MISSION =
   "Our mission is to help you pass for a native Argentinian when you speak Spanish.";
+
+export const SOCIALS = [
+  {
+    name: "Twitter",
+    url: "https://twitter.com/joseprevite",
+    icon: "mdi:twitter",
+  },
+  {
+    name: "YouTube",
+    url: "https://twitter.com/argentinian_spanish",
+    icon: "mdi:youtube",
+  },
+];
